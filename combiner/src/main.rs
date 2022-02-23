c2a0d8163938c4eeca13d84b37ff9524d2c145f5
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 18:16:43 +0300

    Combiner. Destructuring Tuples

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -7,11 +7,19 @@ use std::fs::File;
 use std::io::BufReader;
 
 fn main() {
-    let args = Args::new();
-    println!("`println!` called\n\t{:?}", args);
+    let args: Args = Args::new();
+    println!("'println!' called at the func 'main'\n\targs:{:?}", args);
+
+    let (image_1, image_format_1) = find_image_from_path(args.image_1);
+    let (image_2, image_format_2) = find_image_from_path(args.image_2);
 }
 
 fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    println!(
+        "'println!' called at the func 'find_image_from_path'\n\tpath: {}",
+        path
+    );
+
     let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap;
     let image_format: ImageFormat = image_reader.format().unwrap();
     let image: DynamicImage = image_reader.decode().unwrap();
