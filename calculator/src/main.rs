0ba443c4c10df421edb131fb5e7d2d3dda9c4b41
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 15:15:51 +0300

    understanding the nth() Method

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,7 +1,19 @@
 use std::env::{args, Args};
 
+// cargo run -- 1 + 2
+
 fn main() {
     let mut args: Args = args();
-    let first = args.nth(0);
-    println!("println called: {:?}", first);
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(2).unwrap();
+    let second = args.nth(3).unwrap();
+    println!("println called: {:?} {} {}", first, operator, second);
+}
+
+fn nth(&mut self, n: usize) -> Option<String> {
+    // assume n = 0;
+    // inner = ["1", "2"]
+    self.inner.next(); // "1"
+                       // calling next() again result in second element
+    self.inner.next() //"2"
 }
