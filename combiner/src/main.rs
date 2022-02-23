62af40e3404b6938957a9b1540b3685807ca91c9
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 17:00:38 +0300

    Combiner. Declaring the New Module

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,3 +1,11 @@
+mod args;
+use args::Args;
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args {
+        image_1: String::new(),
+        image_2: String::new(),
+        output: String::new(),
+    };
+    let println!("Hello, world!");
 }
