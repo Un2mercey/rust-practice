d25aca776e610231026d5f337de01cda8d41b813
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 23:33:31 +0300

    Combiner. Combining the Image Pixels

--- combiner/src/helper_functions.rs
+++ combiner/src/helper_functions.rs
@@ -45,3 +45,38 @@ pub fn standardise_size(
         (image_1, image_2.resize_exact(width, height, Triangle))
     }
 }
+
+pub fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
+    let vec_1 = image_1.to_rgba8().into_vec();
+    let vec_2 = image_2.to_rgba8().into_vec();
+
+    alternate_pixels(vec_1, vec_2)
+}
+
+fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
+    // if vec_1.len() == 5, vec! macro will create the same [0,0,0,0,0]
+    let mut combine_data = vec![0u8; vec_1.len()];
+
+    let mut i = 0;
+    while i < vec_1.len() {
+        if i % 8 == 0 {
+            combine_data.splice(i..=i + 3, set_rgba(vec_1, i, i + 3))
+        } else {
+            combine_data.splice(i..=i + 3, set_rgba(vec_2, i, i + 3))
+        }
+        i += 4;
+    }
+    combine_data
+}
+
+fn set_rgba(vec: Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+    let mut rgba: Vec<u8> = Vec::new();
+    for i in start..=end {
+        let val: u8 = match vec.get(i) {
+            Some(d) => d,
+            None => panic!("index out of bounds"),
+        };
+        rgba.push(val);
+    }
+    rgba
+}
