d273232ca9a61e375fb4ecd1c2bbd5201074adab
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 18:36:26 +0300

    Combiner. Resizing Images

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,8 +1,9 @@
 mod args;
 
 use args::Args;
+use image::imageops::Triangle;
 use image::io::Reader;
-use image::{DynamicImage, ImageFormat};
+use image::{DynamicImage, GenericImageView, ImageFormat};
 use std::fs::File;
 use std::io::BufReader;
 
@@ -21,6 +22,8 @@ fn main() -> Result<(), ImageDataErrors> {
         return Err(ImageDataErrors::DifferentImageFormats);
     }
 
+    let (image_1, image_2) = standardise_size(image_1, image_2);
+
     Ok(())
 }
 
@@ -34,4 +37,31 @@ fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
     let image_format: ImageFormat = image_reader.format().unwrap();
     let image: DynamicImage = image_reader.decode().unwrap();
     (image, image_format)
+}
+
+fn get_smallest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
+    let pix_1 = dim_1.0 * dim_1.1;
+    let pix_2 = dim_2.0 * dim_2.1;
+
+    println!(
+        "'println!' called at the func 'get_smallest_dimensions'\n\tpix_1: {}\n\tpix_2: {}",
+        pix_1, pix_2
+    );
+
+    return if pix_1 < pix_2 { dim_1 } else { dim_2 };
+}
+
+fn standardise_size(image_1: DynamicImage, image_2: DynamicImage) -> (DynamicImage, DynamicImage) {
+    let (width, height) = get_smallest_dimensions(image_1.dimensions(), image_2.dimensions());
+
+    println!(
+        "'println!' called at the func 'standardise_size'\n\twidth: {}\n\theight: {}",
+        width, height
+    );
+
+    if image_2.dimensions() == (width, height) {
+        (image_1.resize_exact(width, height, Triangle), image_2)
+    } else {
+        (image_1, image_2.resize_exact(width, height, Triangle))
+    }
 }
