d087dae3881d652ecc6b90543dc72e349a4ecd64
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 18:06:40 +0300

    Combiner. Decoding Images

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,7 +1,19 @@
 mod args;
+
 use args::Args;
+use image::io::Reader;
+use image::{DynamicImage, ImageFormat};
+use std::fs::File;
+use std::io::BufReader;
 
 fn main() {
     let args = Args::new();
     println!("`println!` called\n\t{:?}", args);
+}
+
+fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
+    let image_reader: Reader<BufReader<File>> = Reader::open(path).unwrap;
+    let image_format: ImageFormat = image_reader.format().unwrap();
+    let image: DynamicImage = image_reader.decode().unwrap();
+    (image, image_format)
 }
