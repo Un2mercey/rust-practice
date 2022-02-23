4dcefe6cc7d141d33c472bdb833a95c1b8abda45
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 15:45:23 +0300

    parsing Strings into Floats

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -4,16 +4,16 @@ use std::env::{args, Args};
 
 fn main() {
     let mut args: Args = args();
-    let first = args.nth(1).unwrap();
-    let operator = args.nth(2).unwrap();
-    let second = args.nth(3).unwrap();
-    println!("println called: {:?} {} {}", first, operator, second);
-}
 
-fn nth(&mut self, n: usize) -> Option<String> {
-    // assume n = 0;
-    // inner = ["1", "2"]
-    self.inner.next(); // "1"
-                       // calling next() again result in second element
-    self.inner.next() //"2"
+    let first = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_number = first.parse::<f32>().unwrap();
+    let second_number = second.parse::<f32>().unwrap();
+
+    println!(
+        "println called\n\tresult: {} {} {}",
+        first, operator, second
+    );
 }
