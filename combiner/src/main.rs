ded9d6200a3f2371ffc7e58da5bc4dde1461a612
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 18:19:47 +0300

    Combiner. Enums

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -6,7 +6,11 @@ use image::{DynamicImage, ImageFormat};
 use std::fs::File;
 use std::io::BufReader;
 
-fn main() -> Result<()> {
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataErrors> {
     let args: Args = Args::new();
     println!("'println!' called at the func 'main'\n\targs:{:?}", args);
 
@@ -14,7 +18,7 @@ fn main() -> Result<()> {
     let (image_2, image_format_2) = find_image_from_path(args.image_2);
 
     if image_format_1 != image_format_2 {
-        return Err();
+        return Err(ImageDataErrors::DifferentImageFormats);
     }
 
     Ok(())
