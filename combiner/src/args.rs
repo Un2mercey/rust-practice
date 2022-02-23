2d114095348b73df8bc12a79377d55b733c4c275
Arseniy Markov <zamberg42@gmail.com>
2022-02-23 17:01:52 +0300

    Combiner. Private vs Public

--- combiner/src/args.rs
+++ combiner/src/args.rs
@@ -2,8 +2,8 @@ fn get_nth_arg(n: usize) -> String {
     std::env::args().nth(n).unwrap()
 }
 
-struct Args {
-    image_1: String,
-    image_2: String,
-    output: String,
+pub struct Args {
+    pub image_1: String,
+    pub image_2: String,
+    pub output: String,
 }
